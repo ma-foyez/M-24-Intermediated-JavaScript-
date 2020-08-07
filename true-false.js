7200feac8ac23c09d34270128c160e67f1fee173
ma-foyez <mafoyez-bd@gmail.com>
2020-08-08 00:07:24 +0600

    True Vs False Practise

---
+++ true-false.js
@@ -0,0 +1,30 @@
+//falsy
+// 0
+//"" this is empty array
+// undefined
+//null
+//NaN
+// false
+
+//==================================
+//truthy
+//true if we set variable is true
+// "0", here 0 is a value of string
+// " " here white space is a value of a string;
+
+
+// const num = 4;
+// if (num) {
+//     console.log("condition is true");
+// } else {
+//     console.log("condition is false");
+// }
+
+// const name = "";           // this condition is false
+
+const name = "Fayez"; // this output will be true
+if (name.length) { // if we used string we shout compire it using by length keyword
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
